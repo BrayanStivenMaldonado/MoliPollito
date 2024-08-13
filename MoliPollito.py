3dfdc1bee9526ba1cee86f0f33a9ce38ea290792
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2024-08-13 11:05:46 -0500

    feat: :sparkles: starting to create the json

--- MoliPollito.py
+++ MoliPollito.py
@@ -4,4 +4,17 @@ from os import system
 boolGeneral = True
 
 while boolGeneral == True:
-    input("BIENVENIDO A MOLIPOLLITO")
+    eleccion = int(input("BIENVENIDO A MOLIPOLLITO\n\n1.  \n2.  \n3."))
+
+    if eleccion == 1:
+        print("")
+
+    elif eleccion == 2: 
+        print("")
+    
+    elif eleccion == 3:
+        print("")
+    
+    else:
+        input("Esta no es una opción válida, presione ENTER para continuar\n")
+        system("cls")
